345054925dd7a823b09678c1efa8f829402fd279
Ri-Reyan <rireyan50@gmail.com>
2026-09-16 22:42:00 +0600

    setup proxy in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,14 @@ const nextConfig: NextConfig = {
       { protocol: "https", hostname: "images.unsplash.com", pathname: "/**" },
     ],
   },
+  async rewrites() {
+    return [
+      {
+        source: "/api/v1/:path*", // ফ্রন্টএন্ড থেকে যে রুটে রিকোয়েস্ট পাঠাবে
+        destination: `${process.env.NEXT_PUBLIC_API_URL}/api/v1/:path*`, // তোমার মূল ব্যাকএন্ডের প্রডাকশন URL (সামনের স্ল্যাশ বজায় রাখবে)
+      },
+    ];
+  },
 };
 
 export default nextConfig;
