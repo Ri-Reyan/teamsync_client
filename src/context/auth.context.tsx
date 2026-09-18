9b7b893ac851aba36db5aee448fb2998ef91b7b1
Ri-Reyan <rireyan50@gmail.com>
2026-09-18 22:01:49 +0600

    rm isAuthenticated navigation

--- src/context/auth.context.tsx
+++ src/context/auth.context.tsx
@@ -91,10 +91,6 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
       router.replace("/dashboard");
       return;
     }
-
-    if (isAuthenticated) {
-      router.replace("/dashboard");
-    }
   }, [loading, isAuthenticated, user, pathname, router]);
 
   const value = useMemo(
